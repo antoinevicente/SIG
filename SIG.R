30b22c66148d4bf6650ca71d68388cf424689b56
antoine <123209449+antoinevicente@users.noreply.github.com>
2025-09-15 21:52:57 +0200

    Create SIG.R

---
+++ SIG.R
@@ -0,0 +1,52 @@
+# Installer des packages
+install.packages("arrow","dplyr", "ggplot2", "readr")
+
+# Charger des packages
+library(arrow)
+library(dplyr)
+library(ggplot2)
+library(readr)
+
+# Définir l'espace de travail
+setwd("D:/SIG/")
+
+# Importer les données un fichier .parquet
+bpe <- arrow::read_parquet("./BPE24.parquet")
+
+# Importer les données depuis un fichier .csv
+#bpe <- read_delim("data/BPE24.csv", delim=";")
+
+# Aperçu des données
+head(bpe)
+names(bpe)
+
+# Analyse globale du contenu
+dim(bpe)
+nrow(bpe)
+ncol(bpe)
+
+# Ouvrir le tableau
+#view(bpe)
+
+# Résumé des statistiques descriptives
+summary(bpe)
+
+# Comptage des équipements par commune et par type
+equipements_com <- bpe %>%
+  group_by(DEPCOM, TYPEQU) %>%
+  summarise(nb = n(), .groups = "drop")
+
+# Diversité des équipements par commune
+diversite <- equipements_com %>%
+  group_by(DEPCOM) %>%
+  summarise(nb_types = n_distinct(TYPEQU))
+
+# Compter les équipements par type 
+equipements_type <- bpe %>%
+  count(TYPEQU, sort = TRUE) %>%
+  mutate(prop = round(n / sum(n) * 100, 1))
+
+
+
+
+
